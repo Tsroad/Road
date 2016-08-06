33d6e54239896bc37ac3b29de90fafc311ca8a81
C.ztsiang <cztsiang@gmail.com>
2016-08-06 19:33:11 +0800

    Update GeoFenceActivity.java

--- app/src/main/java/com/tsroad/map/GeoFenceActivity.java
+++ app/src/main/java/com/tsroad/map/GeoFenceActivity.java
@@ -19,8 +19,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.location.LocationManagerProxy;
