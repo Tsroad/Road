11cb9985e3d0901f155a96f29c4ab538d9778ece
C.ztsiang <cztsiang@gmail.com>
2016-08-07 11:11:11 +0800

    Update RouteSearchAdapter.java

--- app/src/main/java/com/tsroad/map/route/RouteSearchAdapter.java
+++ app/src/main/java/com/tsroad/map/route/RouteSearchAdapter.java
@@ -1,14 +1,12 @@
 package com.tsroad.map.route;
 
 import java.util.List;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
 import com.amap.api.services.core.PoiItem;
 import com.tsroad.map.R;
 
