8cad820781af4a96fd3af48286d96b8e8cd31a31
C.ztsiang <cztsiang@gmail.com>
2016-10-15 21:45:27 +0800

    Update Welcome.java

--- app/src/main/java/com/tsroad/map/Welcome.java
+++ app/src/main/java/com/tsroad/map/Welcome.java
@@ -10,8 +10,16 @@ import com.amap.api.services.geocoder.GeocodeSearch;
 import com.amap.api.services.geocoder.RegeocodeResult;
 
 /**
+% @authors Keung Charteris & T.s.road CZQ
+% @version 1.0 ($Revision$)
+% @addr. GUET, Gui Lin, 540001,  P.R.China
+% @contact : cztsiang@gmail.com
+% @date Copyright(c)  2016-2020,  All rights reserved.
+% This is an open access code distributed under the Creative Commons Attribution License, which permits 
+% unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited. 
  * Created by tsroad on 5/6/15.
  */
+
 public class Welcome extends Activity implements  GeocodeSearch.OnGeocodeSearchListener {
     private ImageView welcomeImg = null;
 
