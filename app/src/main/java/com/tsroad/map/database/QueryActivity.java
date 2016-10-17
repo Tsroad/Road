c4c0c43edfd87d6e42b39526c334fd66708e9125
C.ztsiang <cztsiang@gmail.com>
2016-10-17 20:20:04 +0800

    Update QueryActivity.java

--- app/src/main/java/com/tsroad/map/database/QueryActivity.java
+++ app/src/main/java/com/tsroad/map/database/QueryActivity.java
@@ -13,6 +13,13 @@ import android.widget.AdapterView.OnItemClickListener;
 import com.tsroad.map.R;
 
 /**
+% @authors Keung Charteris & T.s.road CZQ
+% @version 1.0 ($Revision$)
+% @addr. GUET, Gui Lin, 540001,  P.R.China
+% @contact : cztsiang@gmail.com
+% @date Copyright(c)  2016-2020,  All rights reserved.
+% This is an open access code distributed under the Creative Commons Attribution License, which permits 
+% unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited. 
  * Created by tsroad on 5/6/15.
  */
  
