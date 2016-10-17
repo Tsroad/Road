4f846003da9335432ad9542d3a56a30ca0c2d4b6
C.ztsiang <cztsiang@gmail.com>
2016-10-17 20:14:56 +0800

    Update OffLineMapUtils.java

--- app/src/main/java/com/tsroad/map/util/OffLineMapUtils.java
+++ app/src/main/java/com/tsroad/map/util/OffLineMapUtils.java
@@ -3,6 +3,16 @@ package com.tsroad.map.util;
 import android.content.Context;
 import android.os.Environment;
 
+/**
+% @authors Keung Charteris & T.s.road CZQ
+% @version 1.0 ($Revision$)
+% @addr. GUET, Gui Lin, 540001,  P.R.China
+% @contact : cztsiang@gmail.com
+% @date Copyright(c)  2016-2020,  All rights reserved.
+% This is an open access code distributed under the Creative Commons Attribution License, which permits 
+% unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited. 
+*/
+
 public class OffLineMapUtils {
 	/**
 	 * 获取map 缓存和读取目录
