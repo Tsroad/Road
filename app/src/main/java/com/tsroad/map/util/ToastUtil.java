38d08cbd07b2ee35c398357dccca9bc3b94ee5cc
C.ztsiang <cztsiang@gmail.com>
2016-10-17 20:15:25 +0800

    Update ToastUtil.java

--- app/src/main/java/com/tsroad/map/util/ToastUtil.java
+++ app/src/main/java/com/tsroad/map/util/ToastUtil.java
@@ -1,10 +1,17 @@
-/**
- * 
- */
 package com.tsroad.map.util;
 
 import android.content.Context;
 import android.widget.Toast;
+
+/**
+% @authors Keung Charteris & T.s.road CZQ
+% @version 1.0 ($Revision$)
+% @addr. GUET, Gui Lin, 540001,  P.R.China
+% @contact : cztsiang@gmail.com
+% @date Copyright(c)  2016-2020,  All rights reserved.
+% This is an open access code distributed under the Creative Commons Attribution License, which permits 
+% unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited. 
+*/
 
 public class ToastUtil {
 
