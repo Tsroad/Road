a8d7f87816872476342db277a8facfc4fac806d1
C.ztsiang <cztsiang@gmail.com>
2016-11-17 19:13:30 +0800

    Update SQLiteActivity.java

--- app/src/main/java/com/tsroad/map/database/SQLiteActivity.java
+++ app/src/main/java/com/tsroad/map/database/SQLiteActivity.java
@@ -12,13 +12,13 @@ import android.widget.Button;
 import com.tsroad.map.R;
 
 /**
-% @authors Keung Charteris & T.s.road CZQ
-% @version 1.0 ($Revision$)
-% @addr. GUET, Gui Lin, 540001,  P.R.China
-% @contact : cztsiang@gmail.com
-% @date Copyright(c)  2016-2020,  All rights reserved.
-% This is an open access code distributed under the Creative Commons Attribution License, which permits 
-% unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited. 
+ @authors Keung Charteris & T.s.road CZQ
+ @version 1.0 ($Revision$)
+ @addr. GUET, Gui Lin, 540001,  P.R.China
+ @contact : cztsiang@gmail.com
+ @date Copyright(c)  2016-2020,  All rights reserved.
+ This is an open access code distributed under the Creative Commons Attribution License, which permits 
+ unrestricted use, distribution, and reproduction in any medium, provided the original work is properly cited. 
 */
 
 public class SQLiteActivity extends Activity {
